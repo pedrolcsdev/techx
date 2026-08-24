ba20cde928da94a1d9c72549ee4f8e8d4278c558
pedrolcsdev <pedrolcsdev0@gmail.com>
2026-08-23 21:25:42 -0300

    first commit

---
+++ java-develop/aula-02/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Pessoa pessoa1 = new Pessoa;
+        pessoa1.falar();
+    }
+}
---
+++ java-develop/aula-02/src/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    public String nome;
+    public int idade;
+    public String corDosOlhos;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public void falar() {
+        System.out.println("falando");
+    }
+}
